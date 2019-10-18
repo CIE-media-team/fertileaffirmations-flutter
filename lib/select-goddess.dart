f7c7ed824d1946406f2a969653e01f085a8ff733
kins-sigm <ks3355@uncw.edu>
2019-10-17 23:07:27 -0400

    select goddess screen file

---
+++ lib/select-goddess.dart
@@ -0,0 +1,66 @@
+import "package:flutter/material.dart"; 
+
+class SelectGoddess extends StatefulWidget{
+  SelectGoddess({Key key}) : super(key: key);
+
+  @override
+  _SelectGoddess createState() => _SelectGoddess();
+}
+
+class _SelectGoddess extends State<SelectGoddess> {
+
+  @override
+  Widget build(BuildContext context){
+return Scaffold(
+          appBar: AppBar(
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: Text("select"),
+          ),
+            body: Center(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            child: Stack(
+              
+              alignment: AlignmentDirectional.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: FittedBox(
+                    fit: BoxFit.cover,
+                    child: Image.asset('assets/images/noleaves2.png'),
+                  ),
+                ),
+                Image.asset('assets/images/cardblank.png'),
+                Padding(padding: EdgeInsets.all(20),
+      
+                
+                      child: AutoSizeText(
+                          
+                            
+                      widget.text,
+                  minFontSize: 20,
+                  maxFontSize: 50,
+                  
+                  style: TextStyle(fontFamily: "fancy", fontSize: 60),
+                  textAlign: TextAlign.center,
+                ))
+          ]
+              
+            
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+  }
+}
+
