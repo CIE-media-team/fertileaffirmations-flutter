362e9e2c55a66473882eb79d3dc4c2eb1ac05ea9
kins-sig <kinsley.sigmund@gmail.com>
2020-03-10 18:42:33 -0400

    official app store submit

--- lib/main.dart
+++ lib/main.dart
@@ -214,6 +214,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Fertile Affirmations',
         theme: ThemeData(
           primaryColor: Color(0xff4F694C),
